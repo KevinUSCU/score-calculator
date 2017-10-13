832c21ab2d5a0e53f73e67c963bd5d3c5b9c8868
Kevin Springer <KevinUSCU@outlook.com>
2017-10-13 10:27:38 -0700

    Write readDataFile function and pass test.

--- main.js
+++ main.js
@@ -1,9 +1,33 @@
-function getWinner(filename) {
+const fs = require('fs');
+const Papa = require('papaparse');
 
+function getWinner(filename) {
+    let playerData = readDataFile(filename);
+    playerData.forEach(function(player) {
+        
+    }, this);
 }
 
 function readDataFile(filename) {
-
+    // Read raw data and convert to utf-8
+    let rawContent = fs.readFileSync(filename, "utf-8");
+    // Parse file with PapaParse
+    const options = {
+        header: false,
+        dynamicTyping: true,
+        skipEmptyLines: true
+    }
+    const parsedData = Papa.parse(rawContent, options).data;
+    // Map to our object style
+    const playerData = parsedData.map(element => {
+        let [name, ...scores] = element;
+        let player = {
+            name,
+            scores
+        }
+        return player;
+    });
+    return playerData;
 }
 
 function sumScores(player) {
